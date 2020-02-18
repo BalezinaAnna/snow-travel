2488cad2e8ef6a5a57cca80cb3ac8d5736e2dd65
Анна <balezinaanna@gmail.com>
2020-02-18 14:22:58 +0300

    Init commit

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+		$('.next').click(function(){
+			var id = $(this).closest('.slider').attr('id');
+			var currentImage = $('#'+id+' .img.curry');
+			var currentImageIndex = $('#'+id+' .img.curry').index();
+			var nextImageIndex = currentImageIndex + 1;
+			var nextImage = $('#'+id+' .img').eq(nextImageIndex);
+			currentImage.removeClass('curry');
+
+			if(nextImageIndex == ($('#'+id+' .img:last').index()+1)){
+				$('#'+id+' .img').eq(0).addClass('curry');
+			} else {
+				nextImage.addClass('curry');
+			}
+		});
+		$('.prev').click(function(){
+			var id = $(this).closest('.slider').attr('id');
+			var currentImage = $('#'+id+' .img.curry');
+			var currentImageIndex = $('#'+id+' .img.curry').index();
+			var prevImageIndex = currentImageIndex - 1;
+			var prevImage = $('#'+id+' .img').eq(prevImageIndex);
+
+			currentImage.removeClass('curry');
+			prevImage.addClass('curry');
+		});
+
+		$('.comments__pad').slick({
+  			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			arrows: true, 
+    		prevArrow: '<div class="slick-prev"><i class="fas fa-long-arrow-alt-left fa-3x"></i></div>', 
+    		nextArrow: '<div class="slick-next"><i class="fas fa-long-arrow-alt-right fa-3x"></i></div>'
+		});
+		//$('a.header__menu__item').click(function(){
+			//var elementClick = $(this).attr('href');
+			//var destination = $(elementClick).offset().top;
+			//$('html:not(:animated),body:not(:animated)').animate({scrollTop: destination}, 800);
+			//return false;
+		//});
+
+
+		 $('a[href^="#"]').click(function(){
+       var target = $(this).attr('href');
+       $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+       return false;
+    });
+		
+ 
+
+});
